7bc95ec30537dcabb6aba03992e1610e40c2ba45
evanjt06 <59384760+evanjt06@users.noreply.github.com>
2021-08-04 17:09:03 -0700

    Initial Commit

---
+++ Lysten/LystenApp.swift
@@ -0,0 +1,17 @@
+//
+//  LystenApp.swift
+//  Lysten
+//
+//  Created by Evan Tu on 8/4/21.
+//
+
+import SwiftUI
+
+@main
+struct LystenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
